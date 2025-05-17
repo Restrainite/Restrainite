a4eef06cf7307d079cd3ec197eb90ca5fa6eb3f3
Zenuru <193092054+zenuru-merps@users.noreply.github.com>
2025-05-16 21:26:34 -0500

    Add additional patch to PreventRunning so it works in VR This should make Lute easier to catch in the MM meetups

--- Restrainite/Patches/PreventRunning.cs
+++ Restrainite/Patches/PreventRunning.cs
@@ -1,3 +1,4 @@
+using Elements.Core;
 using FrooxEngine;
 using HarmonyLib;
 using Restrainite.Enums;
@@ -21,5 +22,13 @@ internal static class PreventRunning
     private static void ScreenLocomotionDirection_Evaluate_Postfix(ScreenLocomotionDirection __instance, float __state)
     {
         __instance.FastMultiplier = __state;
+    }
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(VR_LocomotionDirection), nameof(VR_LocomotionDirection.Evaluate))]
+    private static void VR_LocomotionDirection_Evaluate_Postfix(VR_LocomotionDirection __instance, ref float3? __result)
+    {
+        if (RestrainiteMod.IsRestricted(PreventionType.PreventRunning))
+            __result = __result?.Normalized;
     }
 }
