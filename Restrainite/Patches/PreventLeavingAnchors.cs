de633fdc117d8d91af3f933e6ad227102c7bdff8
SnepDrone <189016534+SnepDrone@users.noreply.github.com>
2025-07-07 20:47:56 +0200

    Fix local world crash, when Prevent Leaving Anchors was on and the local user is in an anchor, and the local user is being forced into another anchor by Protoflux.

--- Restrainite/Patches/PreventLeavingAnchors.cs
+++ Restrainite/Patches/PreventLeavingAnchors.cs
@@ -1,4 +1,5 @@
-﻿using FrooxEngine.CommonAvatar;
+﻿using System.Threading;
+using FrooxEngine.CommonAvatar;
 using HarmonyLib;
 
 namespace Restrainite.Patches;
@@ -6,11 +7,27 @@ namespace Restrainite.Patches;
 [HarmonyPatch]
 internal static class PreventLeavingAnchors
 {
+    private static readonly ThreadLocal<bool> IsAnchoring = new();
+
     [HarmonyPrefix]
     [HarmonyPatch(typeof(AvatarAnchor), nameof(AvatarAnchor.Release))]
     private static bool AvatarAnchor_Release_Prefix(AvatarAnchor __instance)
     {
         return __instance.Engine.WorldManager.FocusedWorld.LocalUser != __instance.AnchoredUser
-               || !Restrictions.PreventLeavingAnchors.IsRestricted;
+               || IsAnchoring.Value || !Restrictions.PreventLeavingAnchors.IsRestricted;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(AvatarAnchor), nameof(AvatarAnchor.Anchor), typeof(AvatarManager))]
+    private static void AvatarAnchor_Anchor_Prefix()
+    {
+        IsAnchoring.Value = true;
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(AvatarAnchor), nameof(AvatarAnchor.Anchor), typeof(AvatarManager))]
+    private static void AvatarAnchor_Anchor_Postfix()
+    {
+        IsAnchoring.Value = false;
     }
 }
