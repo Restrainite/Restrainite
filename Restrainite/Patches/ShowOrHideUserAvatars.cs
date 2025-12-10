b5be340d2db3d1ae36b1eed9448a7b729c38a188
SnepDrone <189016534+SnepDrone@users.noreply.github.com>
2025-12-10 01:34:04 +0100

    Reimplement ShowOrHideUserAvatars to be less resource-intensive and avoid crashing.

--- Restrainite/Patches/ShowOrHideUserAvatars.cs
+++ Restrainite/Patches/ShowOrHideUserAvatars.cs
@@ -1,7 +1,11 @@
-using System.Linq;
-using System.Threading;
+using System.Reflection;
+using Elements.Core;
 using FrooxEngine;
+using FrooxEngine.CommonAvatar;
+using FrooxEngine.PhotonDust;
+using FrooxEngine.UIX;
 using HarmonyLib;
+using ResoniteModLoader;
 using Restrainite.RestrictionTypes.Base;
 
 namespace Restrainite.Patches;
@@ -9,8 +13,7 @@ namespace Restrainite.Patches;
 [HarmonyPatch]
 internal static class ShowOrHideUserAvatars
 {
-    private static readonly ThreadLocal<bool> InUpdateBlocking = new();
-    private static int _alreadyMarkedForUpdate;
+    private static readonly SlowUpdater Updater = new();
 
     internal static void Initialize()
     {
@@ -20,51 +23,248 @@ internal static class ShowOrHideUserAvatars
 
     private static void OnRestrictionChanged(IRestriction restriction)
     {
-        MarkAllUsersDirty();
+        Updater.Start();
     }
 
-    private static void MarkAllUsersDirty()
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(AvatarNameplateVisibilityDriver), nameof(AvatarNameplateVisibilityDriver.ShouldBeVisible),
+        MethodType.Getter)]
+    private static void AvatarNameplateVisibilityDriver_ShouldBeVisible_Postfix(ref bool __result,
+        AvatarNameplateVisibilityDriver __instance)
     {
-        var tick = Engine.Current.UpdateTick;
-        if (Interlocked.Exchange(ref _alreadyMarkedForUpdate, tick) == tick) return;
-        var world = Engine.Current?.WorldManager?.FocusedWorld;
-        world?.RunInUpdates(1, () =>
+        ShouldBeVisible(ref __result, __instance);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(AvatarLiveIndicator), nameof(AvatarLiveIndicator.ShouldBeVisible), MethodType.Getter)]
+    private static void AvatarLiveIndicator_ShouldBeVisible_Postfix(ref bool __result, AvatarLiveIndicator __instance)
+    {
+        ShouldBeVisible(ref __result, __instance);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(GaussianSplatRenderer), nameof(GaussianSplatRenderer.ShouldBeEnabled), MethodType.Getter)]
+    private static void GaussianSplatRenderer_ShouldBeEnabled_Postfix(ref bool __result,
+        GaussianSplatRenderer __instance)
+    {
+        ShouldBeVisible(ref __result, __instance);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Light), nameof(Light.ShouldBeEnabled), MethodType.Getter)]
+    private static void Light_ShouldBeEnabled_Postfix(ref bool __result, Light __instance)
+    {
+        ShouldBeVisible(ref __result, __instance);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(MeshRenderer), nameof(MeshRenderer.ShouldBeEnabled), MethodType.Getter)]
+    private static void MeshRenderer_ShouldBeEnabled_Postfix(ref bool __result, MeshRenderer __instance)
+    {
+        ShouldBeVisible(ref __result, __instance);
+    }
+
+    [HarmonyPatch]
+    private static class ParticleSystemPatch
+    {
+        private static IEnumerable<MethodInfo> TargetMethods()
         {
-            var userList = world.AllUsers;
-            if (userList is null) return;
-            foreach (var slot in userList.Select(user => user?.Root?.Slot))
-                slot?.ForeachComponentInChildren<Component>(c => c?.MarkChangeDirty());
-        });
+            return AccessTools.GetDeclaredMethods(typeof(ParticleSystem)).Where(method => method.Name.Contains("ShouldBeEnabled")).AsEnumerable();
+        }
+        
+        private static void Postfix(ref bool __result, ParticleSystem __instance)
+        {
+            ShouldBeVisible(ref __result, __instance);
+        }
     }
+    
 
-    [HarmonyPrefix]
-    [HarmonyPatch(typeof(User), nameof(User.UpdateBlocking))]
-    private static void User_UpdateBlocking_Prefix()
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(TextRenderer), nameof(TextRenderer.ShouldBeEnabled), MethodType.Getter)]
+    private static void TextRenderer_ShouldBeEnabled_Postfix(ref bool __result, TextRenderer __instance)
     {
-        InUpdateBlocking.Value = true;
+        ShouldBeVisible(ref __result, __instance);
     }
 
     [HarmonyPostfix]
-    [HarmonyPatch(typeof(User), nameof(User.UpdateBlocking))]
-    private static void User_UpdateBlocking_Postfix()
+    [HarmonyPatch(typeof(Canvas), nameof(Canvas.ShouldBeEnabled), MethodType.Getter)]
+    private static void Canvas_ShouldBeEnabled_Postfix(ref bool __result, Canvas __instance)
     {
-        InUpdateBlocking.Value = false;
+        ShouldBeVisible(ref __result, __instance);
     }
 
-    [HarmonyPostfix]
-    [HarmonyPatch(typeof(User), nameof(User.IsRenderingLocallyBlocked), MethodType.Getter)]
-    private static void User_IsRenderingLocallyBlocked_Postfix(ref bool __result, User __instance)
+    private static void ShouldBeVisible(ref bool result, Component component)
     {
-        if (InUpdateBlocking.Value || __result || __instance == __instance.LocalUser) return;
+        if (!result ||
+            (!Restrictions.ShowUserAvatars.IsRestricted && !Restrictions.HideUserAvatars.IsRestricted) || 
+            component.IsLocalElement ||
+            component.Slot?.IsLocalElement != false) return;
+        var user = component.Slot?.ActiveUser;
+        if (user == null) return;
+        if (user.IsLocalUser) return;
         if (Restrictions.ShowUserAvatars.IsRestricted &&
-            !Restrictions.ShowUserAvatars.StringSet.Contains(__instance.UserID))
+            !Restrictions.ShowUserAvatars.StringSet.Contains(user.UserID))
         {
-            __result = true;
+            result = false;
             return;
         }
 
         if (!Restrictions.HideUserAvatars.IsRestricted ||
-            !Restrictions.HideUserAvatars.StringSet.Contains(__instance.UserID)) return;
-        __result = true;
+            !Restrictions.HideUserAvatars.StringSet.Contains(user.UserID)) return;
+        result = false;
+    }
+
+    /// <summary>
+    /// To avoid lagging the game, we update the visibility of all avatars for each user in small chunks. Every update
+    /// cycle, we mark one component in one user dirty. 
+    /// </summary>
+    private sealed class SlowUpdater
+    {
+        private const int TotalComponentCount = 8;
+        private readonly List<RefID> _finishedUsers = [];
+        private string? _currentSessionId;
+        private RefID? _currentUserId;
+        private int _finishedTypesForCurrentUser;
+        private ImmutableStringSet? _hiddenUsers;
+        private bool _hiddenUsersState;
+        private int _isRunning;
+        private ImmutableStringSet? _shownUsers;
+        private bool _shownUsersState;
+
+        public void Start()
+        {
+            var world = Engine.Current?.WorldManager?.FocusedWorld;
+            if (world == null) return;
+            if (Interlocked.Exchange(ref _isRunning, 1) == 1) return;
+            world.RunInUpdates(1, Update);
+        }
+
+        private void Update()
+        {
+            try
+            {
+                var world = Engine.Current?.WorldManager?.FocusedWorld;
+                if (world == null)
+                {
+                    Finish();
+                    return;
+                }
+
+                ResetStateIfNecessary(world);
+
+                var currentUserSlot = CurrentUserSlot(world, _currentUserId);
+
+                if (currentUserSlot == null)
+                {
+                    var userList = world.AllUsers;
+                    currentUserSlot = NextUserSlot(userList);
+                }
+
+                if (currentUserSlot == null || _currentUserId == null)
+                {
+                    Finish();
+                    return;
+                }
+
+                MarkChangeDirty(_finishedTypesForCurrentUser, currentUserSlot);
+                _finishedTypesForCurrentUser++;
+
+                if (_finishedTypesForCurrentUser >= TotalComponentCount)
+                {
+                    _finishedUsers.Add((RefID)_currentUserId);
+                    _currentUserId = null;
+                    _finishedTypesForCurrentUser = 0;
+                }
+
+                world.RunInUpdates(1, Update);
+            }
+            catch (Exception e)
+            {
+                ResoniteMod.Warn("Error while updating user avatars", e);
+                Finish();
+            }
+        }
+
+        private static Slot? CurrentUserSlot(World world, RefID? userId)
+        {
+            if (userId == null) return null;
+            var user = world.GetUser(userId.Value);
+            var currentUserSlot = user?.Root?.Slot;
+            return currentUserSlot;
+        }
+
+        private Slot? NextUserSlot(Dictionary<RefID, User>.ValueCollection userList)
+        {
+            var user = userList.FirstOrDefault(user => !_finishedUsers.Contains(user.ReferenceID));
+            _currentUserId = user?.ReferenceID;
+            _finishedTypesForCurrentUser = 0;
+            return user?.Root?.Slot;
+        }
+
+        private void Finish()
+        {
+            Reset();
+            Interlocked.Exchange(ref _isRunning, 0);
+        }
+
+        private void ResetStateIfNecessary(World world)
+        {
+            if (_shownUsersState == Restrictions.ShowUserAvatars.IsRestricted &&
+                _hiddenUsersState == Restrictions.HideUserAvatars.IsRestricted &&
+                _shownUsers != null && _shownUsers.Equals(Restrictions.ShowUserAvatars.StringSet.Value) &&
+                _hiddenUsers != null && _hiddenUsers.Equals(Restrictions.HideUserAvatars.StringSet.Value) &&
+                _currentSessionId == world.SessionId) return;
+            Reset(world.SessionId);
+        }
+
+        private void Reset(string? sessionId = null)
+        {
+            _shownUsers = Restrictions.ShowUserAvatars.StringSet.Value;
+            _hiddenUsers = Restrictions.HideUserAvatars.StringSet.Value;
+            _shownUsersState = Restrictions.ShowUserAvatars.IsRestricted;
+            _hiddenUsersState = Restrictions.HideUserAvatars.IsRestricted;
+            _finishedUsers.Clear();
+            _currentUserId = null;
+            _finishedTypesForCurrentUser = 0;
+            _currentSessionId = sessionId;
+        }
+
+        /// <summary>
+        /// This list of components has been created by searching for all components that are using the
+        /// <see cref="User.IsRenderingLocallyBlocked"/> property.
+        /// </summary>
+        /// <param name="component">Internal id of the component</param>
+        /// <param name="slot">User Root Slot</param>
+        private static void MarkChangeDirty(int component, Slot slot)
+        {
+            switch (component)
+            {
+                case 0:
+                    slot.ForeachComponentInChildren<AvatarNameplateVisibilityDriver>(c => c?.MarkChangeDirty());
+                    break;
+                case 1:
+                    slot.ForeachComponentInChildren<AvatarLiveIndicator>(c => c?.MarkChangeDirty());
+                    break;
+                case 2:
+                    slot.ForeachComponentInChildren<GaussianSplatRenderer>(c => c?.MarkChangeDirty());
+                    break;
+                case 3:
+                    slot.ForeachComponentInChildren<Light>(c => c?.MarkChangeDirty());
+                    break;
+                case 4:
+                    slot.ForeachComponentInChildren<MeshRenderer>(c => c?.MarkChangeDirty());
+                    break;
+                case 5:
+                    slot.ForeachComponentInChildren<ParticleSystem>(c => c?.MarkChangeDirty());
+                    break;
+                case 6:
+                    slot.ForeachComponentInChildren<TextRenderer>(c => c?.MarkChangeDirty());
+                    break;
+                case 7:
+                    slot.ForeachComponentInChildren<Canvas>(c => c?.MarkChangeDirty());
+                    break;
+                // Remember to update TotalComponentCount when adding new components
+            }
+        }
     }
 }
